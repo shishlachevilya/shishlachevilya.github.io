09fc4395193f3720c9204e47a25310641d11020e
Shishlachev-ilya <mailforwot43@mail.ru>
2019-10-02 21:00:55 +0300

    new project movies

---
+++ movies/precache-manifest.e2c02a4ef9131fd8e3c0ff13880337ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878802b0acaf3d9999ff9e9293e3d3bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7dd04f67f342d5f8ac9",
+    "url": "/static/css/main.3c5bfdcf.chunk.css"
+  },
+  {
+    "revision": "acefdeb19d6d81a53137",
+    "url": "/static/js/2.1ee88072.chunk.js"
+  },
+  {
+    "revision": "a7dd04f67f342d5f8ac9",
+    "url": "/static/js/main.ed155501.chunk.js"
+  },
+  {
+    "revision": "d3e1b94af2168fc39f87",
+    "url": "/static/js/runtime-main.ef150400.js"
+  }
+]);
